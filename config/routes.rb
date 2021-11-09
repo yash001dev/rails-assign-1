1f043d90bf1d0081a81b55a3c080e3af4a48d8a0
yash001dev <y.sanghavi001@gmail.com>
2021-11-09 08:53:15 +0530

    Physician Addedd

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # resources :appoinments
   resources :industries
   resources :patients
-  # resources :physicians
+  resources :physicians
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
